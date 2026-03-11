d8857c4e164994ae55322246fded71507e481d1a
Julieth <julieth89@gmail.com>
2026-03-11 13:19:49 -0500

    Se sube ejercicio 3

---
+++ Ejercicio 3.js
@@ -0,0 +1,8 @@
+
+let productValue = prompt ("Ingrese el valor del producto: ")
+let discount = prompt ("Ingrese el % de descuento: ")
+
+let finalPrice = ProductValue - ((productValueroductValue * discount) / 100)
+
+alert("El precio final del articulo es: "+ finalPrice + " Pesos") 
+
